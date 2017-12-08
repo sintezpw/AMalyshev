881e83edafbec02e2cf466d0e6dbbb2c6589c7a8
aleksmal76 <leha7.6@yandex.ru>
2017-12-09 02:13:24 +0300

    начало разработки на Gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var del = require("del");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var minify = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+
+
+gulp.task("style", function () {
+            gulp.src("less/style.less")
+                .pipe(plumber())
+                .pipe(sass())
+                .pipe(postcss([
+                    autoprefixer()
+]))
+    .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
+        .pipe(server.stream());
+    });
