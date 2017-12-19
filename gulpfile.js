d4839ff019a212da9689d59b71aaaccd339943cb
aleksmal76 <leha7.6@yandex.ru>
2017-12-19 16:34:54 +0300

    начало оформления стилей блока footer

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,10 @@ var browserSync = require('browser-sync');
 var livereload = require('gulp-livereload');
 var plumber = require('gulp-plumber');
 var postcss = require("gulp-postcss");
+var concat = require('gulp-concat');
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var uglify = require('gulp-uglify');
 var del = require("del");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
@@ -17,6 +19,7 @@ var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
 var rename = require("gulp-rename");
 var run = require("run-sequence");
+var concatCss = require('gulp-concat-css');
 
 
 var patch = {
@@ -44,64 +47,64 @@ var patch = {
     },
     clean: './build'
 };
-
-gulp.task('sass', function () { // Создаем таск Sass
-    return gulp.src('sass/**/*.scss') // Берем источник
-        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('css')) // Выгружаем результата в папку css
-        .pipe(livereload())
-        .pipe(browserSync.reload({ stream: true }));
-         // Обновляем CSS на странице при изменении
-});
-gulp.task('browser-sync', function () { // Создаем таск browser-sync
-    browserSync({ // Выполняем browserSync
-        server: { // Определяем параметры сервера
-            baseDir: 'build' // Директория для сервера - build
-        },
-        notify: false // Отключаем уведомления
-    });
-});
-
-
-gulp.task('browser-sync', function () {
-    var files = [
-        'build/html/**/*.html',  ////
-        'html/**/*.html',
-        'build/css/**/*.css',
-        'build/img/**/*.png',
-        'build/js/**/*.js'
-    ];
-
-    browserSync.init(files, {
-        server: {
-            baseDir: 'build'
-        }
-    });
-});
-
-
-
-
-
-gulp.task('watch', ['browser-sync', 'sass', 'html'], function () {
-
-    gulp.watch('sass/**/*scss', ['sass']);
-    gulp.watch('*/.html', browserSync.reload);  ////
-});
+// SaSS
 
 gulp.task("sass", function () {
     gulp.src("sass/main.scss")////
         .pipe(plumber())
         .pipe(sass())
         .pipe(postcss([
-            autoprefixer()
-        ]))
+        autoprefixer()
+]))
         .pipe(gulp.dest("build/css"))
         .pipe(minify())
         .pipe(rename("style.min.css"))
         .pipe(gulp.dest("build/css"))
         .pipe(server.stream());
 });
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+        baseDir: 'build' // Директория для сервера - build
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+gulp.task('browser-sync', function () {
+    var files = [
+        'build/html/**/*.html',  ////
+        'build/css/**/*.css',
+        'build/img/**/*.png',
+        'build/js/**/*.js'
+    ];
+browserSync.init(files, {
+        server: {
+            baseDir: 'build'
+        }
+    });
+});
+gulp.task('watch', ['browser-sync', 'sass', 'html', 'js','sprite'], function () {
+gulp.watch('sass/**/*scss', ['sass']);
+gulp.watch('*/.html', browserSync.reload);  ////
+});
+
+gulp.task('js', function () {
+    gulp.src('js/*.js')
+.pipe(gulp.dest("build/js"))
+        .pipe(minify())
+        
+        .pipe(gulp.dest("build/js"))
+        .pipe(server.stream());
+        
+    gulp.watch('js/*.js', ['uglify']);
+});
+gulp.task('uglify', function () {
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+
 
 gulp.task("normalize", function () {
     gulp.src("sass/normalize.scss")
@@ -114,7 +117,6 @@ gulp.task("normalize", function () {
         .pipe(rename("normalize.min.css"))
         .pipe(gulp.dest("build/css"));
 });
-
 gulp.task("server", function () {
     server.init({
         server: "build/",
@@ -127,9 +129,6 @@ gulp.task("server", function () {
    gulp.watch("img/*.svg", ["svgUpdate"]).on("change", server.reload);
     gulp.watch("*.html", ["html"]).on("change", server.reload);
 });
-
-
-
 gulp.task("html", function () {
     return gulp.src("*.html")
         .pipe(posthtml([
@@ -137,13 +136,19 @@ gulp.task("html", function () {
         ]))
         .pipe(gulp.dest("build"));
 });
+gulp.task('uglify', function () {
+    gulp.src('js/*.js')
+        .pipe(plumber()) // plumber
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
 
 gulp.task("webp", function () {
     return gulp.src("img/**/*.{png,jpg}")
         .pipe(webp({ quality: 90 }))
         .pipe(gulp.dest("img"));
 });
-
 gulp.task("img", function () {
     return gulp.src("img/**/*.{png,jpg,svg}")
         .pipe(imagemin([
@@ -153,7 +158,6 @@ gulp.task("img", function () {
         ]))
         .pipe(gulp.dest("img"));
 });
-
 gulp.task("sprite", function () {
     return gulp.src("img/*.svg")
         .pipe(svgstore({
@@ -162,22 +166,20 @@ gulp.task("sprite", function () {
         .pipe(rename("sprite.svg"))
         .pipe(gulp.dest("build/img"));
 });
-
 gulp.task("clean", function () {
     return del("build");
 });
-
 gulp.task("copy", function () {
     return gulp.src([
         "fonts/**/*.{woff,woff2}",
         "img/**",
-        "js/**"
+        "js/**",
+        "html/**"   ////
     ], {
             base: "."
         })
         .pipe(gulp.dest("build"));
 });
-
 gulp.task("svgUpdate", function (done) {
     run(
         "sprite",
---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $(".modal").fancybox();
+    $(".popup-window__grup").submit(function () { return false; });
+});
+
+
+
+
+
+
+	
